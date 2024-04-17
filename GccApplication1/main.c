82c3ebd5724f6d34277438a1981a0f7608902915
Lukas-k03 <98773707+Lukas-k03@users.noreply.github.com>
2024-04-17 16:14:18 -0400

    update

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -10,9 +10,9 @@
 #include "util/delay.h"
 #include <avr/interrupt.h>
 
-char QUARTER = 16;
-char EIGHT = 8;
-char SIXTEENTH = 4;
+char QUARTER = 4;
+char EIGHT = 2;
+char SIXTEENTH = 1;
 
 //hi paul the code rn only reflects the first c being played PORTD PIN1
 // After you solder all the buttons and conncet them to the 8 realys
@@ -27,21 +27,50 @@ void custom_delay_ms(unsigned int ms) {
 	}
 }
 
+
+
+void shiftOutPORTD(char DATAP, char CLKP, char data){
+		for (char i = 0; i < 8; i++) {
+			// Output the current bit on the data pin
+			if (data & (1 << (7 - i))) {
+				PORTD |= (1 << DATAP);  // Set data pin high
+				} else {
+				PORTD &= ~(1 << DATAP); // Set data pin low
+			}
+			
+			// Pulse the clock pin to shift the next bit
+			PORTD |= (1 << CLKP);    // Set clock pin high
+			PORTD &= ~(1 << CLKP);   // Set clock pin low
+		}
+	}
+void shiftRegisterDisplay(char bar){
+		//data pin SER (1<<0);
+		//update clock latch SRCLK (1<<1);
+		//Output pin RCLK (1<<2);
+		
+		PORTD &= ~(1 << 1); // CLKP low
+		PORTD &= ~(1 << 2); // RCLK low
+		
+		PORTD &= ~(1<<2);
+		shiftOutPORTD(0,1,bar);
+		PORTD |= (1<<2);
+		
+	}
 void playNote(float BPM,char noteDuration, char port){
 	
-	float n = (60000.0 / BPM) / 16 * noteDuration; // Calculate total duration in milliseconds
-	PORTD |= (1<<port);
+	float n = ((60000.0 / BPM) / 4) * noteDuration; // Calculate total duration in milliseconds
+	shiftRegisterDisplay(1<<port);
+	custom_delay_ms(n/2); // hey dont remove the delay - spencer
+	shiftRegisterDisplay(0);
 	custom_delay_ms(n/2);
-	PORTD &= ~(1<<port);
-	custom_delay_ms(n/2);
-	
 	}
-	
 int main(void)
 {
     while (1) 
     {
 		DDRD = 0xFF;
+		
+		PORTD |= (1<<3);
 		playNote(100,QUARTER,0);
 		playNote(100,QUARTER,1); //dont use the sixteenth note it will break the realy
 		playNote(100,QUARTER,2);
@@ -50,10 +79,42 @@ int main(void)
 		playNote(100,QUARTER,5); //dont use the sixteenth note it will break the realy
 		playNote(100,QUARTER,6);
 		playNote(100,QUARTER,7);
+		PORTD &= ~(1<<3);
+		PORTD |= (1<<4);
+		playNote(100,QUARTER,0);
+		playNote(100,QUARTER,1); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,2);
+		playNote(100,QUARTER,3);
+		playNote(100,QUARTER,4);
+		playNote(100,QUARTER,5); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,6);
+		playNote(100,QUARTER,7);
+		PORTD &= ~(1<<4);
+		PORTD |= (1<<5);
+		playNote(100,QUARTER,0);
+		playNote(100,QUARTER,1); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,2);
+		playNote(100,QUARTER,3);
+		playNote(100,QUARTER,4);
+		playNote(100,QUARTER,5); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,6);
+		playNote(100,QUARTER,7);
+		PORTD &= ~(1<<5);
+		PORTD |= (1<<6);
+		playNote(100,QUARTER,0);
+		playNote(100,QUARTER,1); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,2);
+		playNote(100,QUARTER,3);
+		playNote(100,QUARTER,4);
+		playNote(100,QUARTER,5); //dont use the sixteenth note it will break the realy
+		playNote(100,QUARTER,6);
+		playNote(100,QUARTER,7);
+		PORTD &= ~(1<<6);
+		
 		
 		}
 		
-		
+		//the problem is the SIPO is taking time to rese
 		
 }
 
